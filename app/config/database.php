b662848c928a3e67b9bb87cb04ad326254fb62ac
G4brym <g4bryrm98@hotmail.com>
2015-02-07 11:46:21 +0000

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'laravel',
-			'username'  => 'laravel',
-			'password'  => 'Pzcq9cbMpPxzhnUT',
+			'database'  => 'wildexame',
+			'username'  => 'wildexame',
+			'password'  => 'KtVeXSW2PEuNNTDK',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
